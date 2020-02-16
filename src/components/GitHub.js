6368e1fee230031a40d91451b23e91ab9040c0d9
Sarah86 <sarah.goncalves@gmail.com>
2020-02-16 22:04:39 +0100

    functioning app

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -4,99 +4,93 @@ import axios from "axios" //Guaranteed compatibility between browsers
 const GitHub = () => {
 
     const user = "Sarah86";
-    const repo = "mandetta";
+    const repo = "cop";
     const my_token = "3633af72eda1cb9816106830f0b22320a5028e45"
 
     const [commits, setCommits] = useState([]);
 
-    const [shaList, setShaList] = useState([]);
+    const [tree_sha, setTree_sha] = useState("");
 
+    function getTreeList(shaNumber) {
 
-    const [tree_sha, setTree_sha] = useState([]);
-
-    const [uniqueSha, setUniqueSha] = useState(null);
-
-    async function getTreeList(uniqueSha) {
-
-        console.log(uniqueSha)
-
-        await axios.get(`https://api.github.com/repos/${user}/${repo}/git/trees/${uniqueSha}`, {
+        axios.get(`https://api.github.com/repos/${user}/${repo}/git/trees/${shaNumber}`, {
             'headers': {
                 'Authorization': `token ${my_token}`
             }
-            })
+        })
             .then(response => {
-                setTree_sha(response.data.tree);
-                console.log(tree_sha)
+                const shaTree = response.data
+                setTree_sha(shaTree);
+                console.log(response.data)
             })
             .catch(error => {
                 console.log(error)
             })
-}
+    }
 
 
-useEffect(() => {
-    async function getCommits() {
-        await axios.get(`https://api.github.com/repos/${user}/${repo}/commits`, {
-            'headers': {
-                'Authorization': `token ${my_token}`
-            }
+    useEffect(() => {
+        async function getCommits() {
+            await axios.get(`https://api.github.com/repos/${user}/${repo}/commits`, {
+                'headers': {
+                    'Authorization': `token ${my_token}`
+                }
             })
-            .then(response => {
-                console.log(response);
-                setCommits(response.data);
-                const concatSha = response.data.map(commit => commit.sha);
-                setShaList(concatSha);
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    };
+                .then(response => {
+                    console.log(response);
+                    setCommits(response.data);
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        };
 
 
-    getCommits();
+        getCommits();
 
-}, [])
+    }, [])
 
-return (
-    <div>
-        <h1>Github</h1>
-        <h2>User: {user} </h2>
-        <h3>Repository: {repo} </h3>
+    return (
         <div>
-            {commits.map((commit, i) => (
-                <div key={i}>
-                    <h3>Commit n° {i}</h3>
-                    <ul>
-                        <li>Author: <strong>{commit.commit.author.name}</strong></li>
-                        <li onClick={() => {
-                            setUniqueSha(commit.sha);
-                            getTreeList(uniqueSha);
-                            }}
-                            
-                            >
-                            Sha: {commit.sha}
-                        </li>
-                        <li>
-                            Modified Files
-                            {/* <ul>
-                                {tree_sha.map(file => {
-                                   <li> {file.path} </li>  
-                                })}                       
-                            </ul> */}
-                        </li>
-                    </ul>
-                </div>
-            ))}
-            {/* <ul>
-                {tree_sha.map(file => {
-                    <li> {file.path} </li>  
-                })}                       
-            </ul> */}
-        </div>
-    </div>
+            <h1>Github</h1>
+            <h2>User: {user} </h2>
+            <h3>Repository: {repo} </h3>
+            <div>
+                {commits.map((commit, i) => (
+                    <div key={i}>
+                        <h3>Commit n° {i}</h3>
+                        <ul>
+                            <li>
+                                <strong>Author:</strong> {commit.commit.author.name}
+                            </li>
+                            <li>
+                                <strong>When:</strong> {commit.commit.author.date}</li>
+                            <li><strong>Message:</strong> {commit.commit.message}</li>
 
-)
+                            <li>
+                                <strong>Sha:</strong> {commit.sha}
+                            </li>
+                            <li onClick={() => getTreeList(commit.sha)}
+                                style={{cursor: 'pointer'}}>
+                                <strong>Modified Files + </strong>
+                                <ul>
+                                    
+                                    {
+                                        (commit.sha === tree_sha.sha)
+                                        &&
+                                        (tree_sha.tree.map(file => (
+                                            <li key={i}>{file.path}</li>
+                                        )))
+                                    }
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    )
 
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,11 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import Box from '@material-ui/core/Box';
-import HudlApp from "../components/Map"
-import ApiTest from "../components/ApiTest"
-import PlacesApi from "../components/Places"
-import TwitterApp from "../components/Twitter"
 import GitHub from "../components/GitHub"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    {/* <HudlApp/>
-    <ApiTest/>
-    <PlacesApi/> */}
-    {/* <TwitterApp/> */}
     <GitHub/>
   </Layout>
 )
