a046ee3925de75b699f521278080761961aaea61
Sarah86 <sarah.goncalves@gmail.com>
2020-02-16 06:32:41 +0100

    github tree

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -11,75 +11,92 @@ const GitHub = () => {
 
     const [shaList, setShaList] = useState([]);
 
+
     const [tree_sha, setTree_sha] = useState([]);
 
+    const [uniqueSha, setUniqueSha] = useState(null);
 
+    async function getTreeList(uniqueSha) {
 
-    const getCommits = () => {
-        axios.get(`https://api.github.com/repos/${user}/${repo}/commits`, {
+        console.log(uniqueSha)
+
+        await axios.get(`https://api.github.com/repos/${user}/${repo}/git/trees/${uniqueSha}`, {
             'headers': {
                 'Authorization': `token ${my_token}`
             }
-        })
+            })
             .then(response => {
-                console.log(response);
-                setCommits(response.data);
-                const concatSha = response.data.map(commit => commit.sha)
-                setShaList(concatSha)
+                setTree_sha(response.data.tree);
+                console.log(tree_sha)
             })
             .catch(error => {
                 console.log(error)
             })
-    }
+}
 
-    const getTreeList = () => {
-    
-    //    for (const sha of setShaList) {
-    //        console.log(sha)
-    //    }
 
-        // axios.get(`https://api.github.com/repos/${user}/${repo}/git/trees/${sha}`, {
-        // 'headers': {
-        //     'Authorization': `token ${my_token}`
-        // }
-        // })
-        // .then(response => {
-        //     console.log(response);
-        // })
-        // .catch(error => {
-        //     console.log(error)
-        // })
-    }
+useEffect(() => {
+    async function getCommits() {
+        await axios.get(`https://api.github.com/repos/${user}/${repo}/commits`, {
+            'headers': {
+                'Authorization': `token ${my_token}`
+            }
+            })
+            .then(response => {
+                console.log(response);
+                setCommits(response.data);
+                const concatSha = response.data.map(commit => commit.sha);
+                setShaList(concatSha);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    };
 
-    useEffect(() => {
-        getCommits();
-        getTreeList();
-    }, [setCommits])
 
-    return (
+    getCommits();
+
+}, [])
+
+return (
+    <div>
+        <h1>Github</h1>
+        <h2>User: {user} </h2>
+        <h3>Repository: {repo} </h3>
         <div>
-            <h1>Github</h1>
-            <h2>User: {user} </h2>
-            <h3>Repository: {repo} </h3>
-            <div>
-                {commits.map((commit, i) => (
-                    <div key={i}>
-                        <h3>Commit n° {i}</h3>
-                        <ul>
-                            <li>Author: <strong>{commit.commit.author.name}</strong></li>
-                            <li>
-                                Sha: {commit.sha}
-                            </li>
-                            <li>
-                                Tree: {}
-                            </li>
-                        </ul>
-                    </div>
-                ))}
-            </div>
+            {commits.map((commit, i) => (
+                <div key={i}>
+                    <h3>Commit n° {i}</h3>
+                    <ul>
+                        <li>Author: <strong>{commit.commit.author.name}</strong></li>
+                        <li onClick={() => {
+                            setUniqueSha(commit.sha);
+                            getTreeList(uniqueSha);
+                            }}
+                            
+                            >
+                            Sha: {commit.sha}
+                        </li>
+                        <li>
+                            Modified Files
+                            {/* <ul>
+                                {tree_sha.map(file => {
+                                   <li> {file.path} </li>  
+                                })}                       
+                            </ul> */}
+                        </li>
+                    </ul>
+                </div>
+            ))}
+            {/* <ul>
+                {tree_sha.map(file => {
+                    <li> {file.path} </li>  
+                })}                       
+            </ul> */}
         </div>
+    </div>
 
-    )
+)
 
 }
 
